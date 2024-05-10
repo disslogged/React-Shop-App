d259f1e29441958e73b74decebb63a99c7220bbe
disslogged <disslogged@gmail.com>
2024-05-10 08:30:46 +0330

    refactor and update file structure

---
+++ src/utils/constants/data.ts
@@ -0,0 +1,8 @@
+export const categoriesList = [
+  { id: 1, name: "All" },
+  { id: 2, name: "Clothes" },
+  { id: 3, name: "Electronics" },
+  { id: 4, name: "Furniture" },
+  { id: 5, name: "Shoes" },
+  { id: 6, name: "Miscellaneous" },
+];
